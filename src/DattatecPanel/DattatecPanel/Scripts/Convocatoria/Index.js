0868675ba874bea9782ef2b08963c1316ab5e36b
Jordan Davila <jdavila93.jdt@gmail.com>
2017-11-11 17:18:00 -0500

    Ordenar los archivos JavaScript

--- src/DattatecPanel/DattatecPanel/Scripts/Convocatoria/Index.js
+++ src/DattatecPanel/DattatecPanel/Scripts/Convocatoria/Index.js
@@ -2,10 +2,10 @@
     var Convocatoria = (function () {
         function Convocatoria() { };
         Convocatoria.prototype.PageLoad = function () {
-            var s = suspender == undefined ? false : suspender;
-            if (!s || s == undefined) {
+            //var s = suspender == undefined ? false : suspender;
+            //if (!s || s == undefined) {
                 gInputsFormatoFecha("FechaInicio,FechaFin,fechaInicioIndex,FechaFinIndex");
-            }
+            //}
             Convocatoria.prototype.dataGrid();
             Convocatoria.prototype.buscar();
             Convocatoria.prototype.agregarEventos();
@@ -75,10 +75,6 @@
                 },
                 success: function (data) {
                     gMostrarResultadoBusqueda(data.rows, "#dgConvocatoriaProveedores");
-
-                    $(".itemSuspender").on('click', function () {
-                        Convocatoria.prototype.frmSuspender();
-                    });
                 },
                 error: function () {
                     gMensajeErrorAjax();
@@ -86,56 +82,9 @@
             });
         }
 
-        Convocatoria.prototype.Guardar = function () {
-            var fini = $("#FechaInicio").val();
-            var ffin = $("#FechaFin").val();
-            var mensaje = ValidarFechaInicio_Fin(fini, ffin);
-            if (mensaje != "") { return gMensajeInformacion(mensaje); }
-
-            gMensajeConfirmacion("¿Esta seguro de registrar?", function () {
-                Convocatoria.prototype.GuardarConvocatoria();
-            });
-            return false;
-        };
-
-        Convocatoria.prototype.GuardarConvocatoria = function () {
-            var frmData = new FormData(document.getElementById('frmNuevoConvocatoria'));
-            $.ajax({
-                url: globalRutaServidor + "Convocatoria/Nuevo",
-                type: 'POST',
-                async: false,
-                contentType: false,
-                processData: false,
-                data: frmData,
-                success: function (data) {
-                    if (data.statusCode == 200) {
-                        if (data.mensajeInfo == "") {
-                            var callback = function () {
-                                $("#btnCancelar").click();
-                            };
-                            gMensajeInformacionConCallback(data.mensaje, callback);
-                        } else {
-                            gMensajeInformacion(data.mensajeInfo);
-                        }
-                    } else {
-                        gMensajeInformacion('Ocurrio un error.');
-                    }
-                },
-                error: function () {
-                    gMensajeErrorAjax();
-                }
-            });
-        };
-
         Convocatoria.prototype.agregarEventos = function () {
-            $("#btnGuardar").on('click', function () {
-                Convocatoria.prototype.Guardar();
-            });
             $("#btnConsultar").on('click', function () {
                 Convocatoria.prototype.buscar();
-            });
-            $("#btnSuspender").on('click', function () {
-                Convocatoria.prototype.GuardarSuspension();
             });
             $("#nroConvocatoria").keypress(function (e) {
                 return (e.keyCode >= 48 && e.keyCode <= 57)
@@ -146,51 +95,6 @@
             $("#FechaFinIndex").keypress(function (e) {
                 return ((e.keyCode >= 48 && e.keyCode <= 57) || e.keyCode == 47)
             });
-        }
-
-        Convocatoria.prototype.frmSuspender = function () {
-            $.ajax({
-                url: globalRutaServidor + "Convocatoria/Suspender",
-                type: "GET",
-                data: null,
-                success: function (contenido) {
-                    gAbrirModal(contenido);
-                },
-                error: function (request, status, error) {
-                    gMensajeErrorAjax();
-                }
-            });
-            return false;
-        };
-
-        Convocatoria.prototype.GuardarSuspension = function () {
-
-            var obs = $("#ObservacionSuspension").val();
-            if (obs == "") { return gMensajeInformacion("Ingrese una observación."); }
-
-            gMensajeConfirmacion("¿Esta seguro de suspender?", function () {
-                var convocatoria = $('#frmSuspension').serializeFormJSON();
-                $.ajax({
-                    url: globalRutaServidor + "Convocatoria/Suspender",
-                    type: 'POST',
-                    async: false,
-                    data: { entidad: convocatoria },
-                    success: function (data) {
-                        if (data.statusCode == 200) {
-                            var callback = function () {
-                                $("#btnCancelar").click();
-                            };
-                            gMensajeInformacionConCallback(data.mensaje, callback);
-                        } else {
-                            gMensajeInformacion('Ocurrio un error.');
-                        }
-                    },
-                    error: function () {
-                        gMensajeErrorAjax();
-                    }
-                });
-            });
-            return false;
         };
 
         return Convocatoria;
---
+++ src/DattatecPanel/DattatecPanel/Scripts/Convocatoria/Nuevo.js
@@ -0,0 +1,95 @@
+﻿(function () {
+    var NuevaConvocatoria = (function () {
+        function NuevaConvocatoria() { };
+        NuevaConvocatoria.prototype.loadPage = function () {
+            var s = suspender == undefined ? false : suspender;
+            if (!s || s == undefined) {
+                gInputsFormatoFecha("FechaInicio,FechaFin,fechaInicioIndex,FechaFinIndex");
+            }
+            NuevaConvocatoria.prototype.agregarEventos();
+        };
+
+        NuevaConvocatoria.prototype.Guardar = function () {
+            var fini = $("#FechaInicio").val();
+            var ffin = $("#FechaFin").val();
+            var mensaje = ValidarFechaInicio_Fin(fini, ffin);
+            if (mensaje != "") { return gMensajeInformacion(mensaje); }
+
+            gMensajeConfirmacion("¿Esta seguro de registrar?", function () {
+                NuevaConvocatoria.prototype.GuardarConvocatoria();
+            });
+            return false;
+        };
+
+        NuevaConvocatoria.prototype.GuardarConvocatoria = function () {
+            var frmData = new FormData(document.getElementById('frmNuevoConvocatoria'));
+            $.ajax({
+                url: globalRutaServidor + "Convocatoria/Nuevo",
+                type: 'POST',
+                async: false,
+                contentType: false,
+                processData: false,
+                data: frmData,
+                success: function (data) {
+                    if (data.statusCode == 200) {
+                        if (data.mensajeInfo == "") {
+                            var callback = function () {
+                                $("#btnCancelar").click();
+                            };
+                            gMensajeInformacionConCallback(data.mensaje, callback);
+                        } else {
+                            gMensajeInformacion(data.mensajeInfo);
+                        }
+                    } else {
+                        gMensajeInformacion('Ocurrio un error.');
+                    }
+                },
+                error: function () {
+                    gMensajeErrorAjax();
+                }
+            });
+        };
+
+        NuevaConvocatoria.prototype.GuardarSuspension = function () {
+
+            var obs = $("#ObservacionSuspension").val();
+            if (obs == "") { return gMensajeInformacion("Ingrese una observación."); }
+
+            gMensajeConfirmacion("¿Esta seguro de suspender?", function () {
+                var convocatoria = $('#frmSuspension').serializeFormJSON();
+                $.ajax({
+                    url: globalRutaServidor + "Convocatoria/Suspender",
+                    type: 'POST',
+                    async: false,
+                    data: { entidad: convocatoria },
+                    success: function (data) {
+                        if (data.statusCode == 200) {
+                            var callback = function () {
+                                $("#btnCancelar").click();
+                            };
+                            gMensajeInformacionConCallback(data.mensaje, callback);
+                        } else {
+                            gMensajeInformacion('Ocurrio un error.');
+                        }
+                    },
+                    error: function () {
+                        gMensajeErrorAjax();
+                    }
+                });
+            });
+            return false;
+        };
+
+        NuevaConvocatoria.prototype.agregarEventos = function () {
+            $("#btnGuardar").on('click', function () {
+                NuevaConvocatoria.prototype.Guardar();
+            });
+            $("#btnSuspender").on('click', function () {
+                NuevaConvocatoria.prototype.GuardarSuspension();
+            });
+        };
+        return NuevaConvocatoria;
+    }());
+    var nuevaConvocatoria = new NuevaConvocatoria();
+    nuevaConvocatoria.loadPage();
+}());
